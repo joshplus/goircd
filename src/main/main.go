ed23250fcbe60352cb62d8dc5c0cf841fff47b08
Josh Abraham <josh@verajosh.com>
2015-04-23 19:20:21 -0400

    Finished adding comments

--- src/main/main.go
+++ src/main/main.go
@@ -251,35 +251,58 @@ func main() {
         }
 }
 
+/*
+ * handle_conn takes a network connection and a user ID. This ID is a serial number for users
+ * who connect to the server. It is put in the user object which is created and used for comparisons.  
+ */
 func handle_conn(conn net.Conn, id int){
+       //Make sure that the connection is closed when (and however) the function ends
        defer conn.Close()
        defer fmt.Println("Disconnected:", id);
+       
+       //Create a user struct for this user
        var usr user
        usr.id=id
        usr.nickname=""
        usr.channel = make(chan message, 25)
+       
+       //ensure that this user leaves all chat rooms when disconnected
        defer part_all(&usr)
+       
+       //Pick a random nickname for the initial connection
+       //this is important for clients understanding welcome messages
+       //if there is a nickname collision
        for !change_nick(&usr,randNick()) {}
        fmt.Println("Got connection #", id);
+       
+       //spawn another goroutine to handle inbound messages
        reader := bufio.NewReader(conn)
 	   go handle_in_conn(&usr, reader)
+	   
+	   //Send the initial connection message
 	   conn.Write([]byte("NOTICE AUTH :*** Welcome to the server!\r\n"))
+	   
+	   //This is the outbound message event loop. Look for messages to send out and send them
        for  {
       	  	imsg, ok := <- usr.channel
       	  	if !ok { break }
       	  	_, err := conn.Write(imsg.message)
-      	  	_, err2 := conn.Write([]byte("\r\n"))
+      	  	_, err2 := conn.Write([]byte("\r\n"))//messages are handled internally without the newline
+      	  	//exit on error - this may be caused by a closed socket. It is intentionally invoked by the 
+      	  	//inbound conneciton handler 
       	  	if err != nil || err2 != nil {
       	  		break
       	  	}
        }		
 }
 
+//handlie_in_conn
+//handle inbound connections from the user.  
 func handle_in_conn(usr *user, reader *bufio.Reader) {
-	for {
+	for { //When a message comes in, parse it 
 		n:=false
 		buf, n, err := reader.ReadLine()
-      	if err != nil { break }
+      	if err != nil { break } //on error break and force  the outbound to fail
       	if (!n) {
     		parse_input(string(buf), usr)
       	} else {
@@ -289,27 +312,38 @@ func handle_in_conn(usr *user, reader *bufio.Reader) {
      }
 	 //We are no longer reading due to a read error
 	 //send a message to the conn_hanler to force an error on
-	 //write and a quick disconnection
+	 //write which ensures a quick disconnection
 	 var msg message
      msg.message = []byte("Bye! I'm not listening any more!")
      defer func() {recover()}() //If someone typed /quit,the channel will be closed so recover to keep from blowing up
      usr.channel <- msg
 }
 
+//Parse input takes a string which represnet input from the client
+//it parses it and decides what to do with it. Much of the RFC-specific behavior
+//is in this behemoth of a function!
 func parse_input(line string, usr *user){
-	fmt.Printf("<USER %d: %s\n",usr.id, line)
+	fmt.Printf("<USER %d: %s\n",usr.id, line) //Debug output of what the user saw
+	
+	//Split line into parts. The first token should be the command (verb)
+	//additional tokens are given separated by spaces
+	//the final token starts with a : which denotes a token that may have spaces (used for messages)
+	//I do not handle all possible command parameters. This should be redone as a regex.
 	comstr := strings.SplitAfterN(line, " ", 2)
 	c := strings.ToLower(comstr[0]) //c == command
 	p1:=""; mb:=""; var p1t, mbt []string //deal with different length arguments
 	if len(comstr) > 1 { p1t = strings.SplitAfterN(comstr[1], " ", 2)} //param 1
-	if len(p1t) > 0 {p1=strings.Trim(strings.Title(p1t[0])," \r\n")}
-	if len(comstr)  > 1 { mbt = strings.SplitAfterN(comstr[1], ":", 2) } //message body
+	if len(p1t) > 0 {p1=strings.Trim(strings.Title(p1t[0])," \r\n")} //set parameter 1 to title case
+	if len(comstr)  > 1 { mbt = strings.SplitAfterN(comstr[1], ":", 2) } //locate the message body
 	if len(mbt) > 1 {mb=mbt[1]}
+	
+	//In this switch statement, I look at the verb with the space at the end not stripped out to improve efficnency
+	//Commands that are just the verb (list) won't necessary have the space
 	switch c {
-		case "nick ":
-			if (p1 == "") {break}
-			oldnick := usr.nickname
-			if (change_nick(usr, p1)){
+		case "nick ": //set the user's nickname
+			if (p1 == "") {break} //no nickname was provided
+			oldnick := usr.nickname //store old nick because you need to provide an accurate "from" message
+			if (change_nick(usr, p1)){ //if we can change nicks, make appropriate updates
 				m := message {source: &server_src, message: []byte(fmt.Sprintf(":%s NICK %s\r\n", oldnick, p1))}
 				if true || usr.nickname!="" { usr.channel <- m }
 				for _, x := range usr.rooms {
@@ -318,82 +352,85 @@ func parse_input(line string, usr *user){
 			} else {
 				server_msg(usr, "433", ":Nickname already in use");
 			}
-		case "privmsg ":
+		case "privmsg ": //this is how you send messages between users and users or other rooms
 			if p1[0] == '#' || p1[0] == '&' {
 				send_room(p1, usr, fmt.Sprintf(":%s PRIVMSG %s :%s\r\n", usr.nickname, p1, mb))
 			} else {
 				priv_msg_usr(usr, p1, mb)
 			}
-		case "topic ":
-			room := room_list[p1]
+		case "topic ": //change the room topic
+			room_mutex.RLock()
+			room := room_list[p1] //this guy isn't threadsafe!
+			room_mutex.RUnlock()
 			if room != nil {
 				room.topic=mb
-				//TOPIC #gobomo :irc client research (foamer2)
+				//send channel topic update to all users in the room
 				m := message {source: usr, message: []byte(fmt.Sprintf(":%s TOPIC %s :%s\r\n", usr.nickname, p1, mb))}
 				if usr.nickname!="" { usr.channel <- m }
 				for _, x := range usr.rooms {
 					x.send_room(&m)
 				}
 			}
-		case "part ":
+		case "part ": //leave a chatroom
 			if len(p1) == 0 { return }
-			if p1[0] != '#' && p1[0] != '&' {p1 = "#" + p1}
+			if p1[0] != '#' && p1[0] != '&' {p1 = "#" + p1} //force the room to fit the channel format (start with # or &)
 			part_room(p1, usr)
 			usr.channel <- message {source: &server_src, message: []byte(fmt.Sprintf(":%s PART %s\r\n", usr.nickname, p1))}
-		case "join ":
+		case "join ": //join a chataroom
 			if p1[0] != '#' && p1[0] != '&' {p1 = "#" + p1}
 			for _,r := range usr.rooms { //don't join a room twice
 				if r.name == p1 {return}
 			}
-			room := join_room(p1, usr)
+			room := join_room(p1, usr) //join the room and update everyone that you're there
 			send_room(p1, usr, fmt.Sprintf(":%s JOIN %s\r\n", usr.nickname, p1))
 			usr.channel <- message {source: &server_src, message: []byte(fmt.Sprintf(":%s JOIN %s\r\n", usr.nickname, p1))}
 			server_msg(usr, "332", p1 + " :" + room.topic)
-			fallthrough
+			fallthrough //Give the list of users when someone joins a room. Not in the RFC but it is expected behavior
 		case "names ":
-			room := room_list[p1]
-			if (room != nil){
+			room_mutex.RLock()
+			room := room_list[p1] //this guy isn't threadsafe
+			room_mutex.RUnlock()
+			if (room != nil){ //if the room exists, send the neames
 					nstr := " = " + room.name + " :"
 					for _, u := range room.users {
 						if (len(nstr) + len(u.nickname) + 2) < 450 { 
 							nstr = nstr + u.nickname + " "
 						} else {
-							server_msg(usr, "353",nstr);
+							server_msg(usr, "353",nstr); //names list ID
 							nstr = " = " + usr.nickname + " :"
 						}
 					}
 					if (len(nstr) > 0){
-						server_msg(usr, "353",nstr);
+						server_msg(usr, "353",nstr); //continue names if there are any left in the buffer
 					}
 					nstr = usr.nickname + " " + p1 + " :End of /names list"
-					server_msg(usr, "366",nstr);
-			} else {
-				/*nstr := " = " + room.name + " :"
-				server_msg(usr, "353",nstr);
-				nstr = usr.nickname + " " + p1 + " :End of /names list"
-				server_msg(usr, "366",nstr);*/
+					server_msg(usr, "366",nstr); //366 = end names list
 			} 
-		case "who ":
-			room := room_list[p1]
+		case "who ": //give details about users in a room - this is like names but with more details
+			room_mutex.RLock()
+			room := room_list[p1] //this guy is still not threadsafe - that's why we keep locking him up!
+			room_mutex.RUnlock()
 			if (room != nil){
 				for _, u := range room.users {
+					//output one name per line with details. Most of the additional details are simulated such as Hx or the server name
 					server_msg(usr, "352",room.name + " " + u.nickname + " example.net server " + u.nickname +  " Hx :0 " + u.nickname);
 				}
 				server_msg(usr, "315", room.name + " :End of /WHO list");
 			}
-		case "list ", "list":
-			rl:=room_list
+		case "list ", "list": //list channels that exist. List may be given with no other parameters
+			room_mutex.RLock()
+			rl:=room_list //I only copy this because server_msg could possibly block if there is a bug or heavy load which would break join/part
+			room_mutex.RUnlock()
 			server_msg(usr, "321", "Channel :Users  Name");
 			for _, room := range rl{
 				nstr := fmt.Sprintf("%s %d :%s", room.name, len(room.users), room.topic) 
 				server_msg(usr, "322", nstr);
 			}
 			server_msg(usr, "323", " :End of /LIST");
-			
-		case "ping ":
+		case "ping ": //RFC requires a PING message to respond with PONG providing the same parameter
 			usr.channel <- message {message: []byte(fmt.Sprintf("PONG %s\r\n", p1))}
 			fmt.Printf(">user %d: %s\n",usr.id, fmt.Sprintf("PONG %s\r\n", p1))
-		case "user ":
+		case "user ": //when a user logs in, send all of the "welcome" headers. Many clients halt without this.
 			resetnick := false
 			if usr.nickname == "" { usr.nickname="*"; resetnick=true }
 			server_msg(usr, "001","Thanks for connecting!");
@@ -406,28 +443,29 @@ func parse_input(line string, usr *user){
 			server_msg(usr, "372","My MOTD");
 			server_msg(usr, "376","End of /MOTD");
 			if resetnick { usr.nickname="" }
-		case "pass ":
+		case "pass ": //clients will send pass, they just need a repsonse to continue
 			server_msg(usr, "462",":Unauthorized command (already registered)")
-		case "notice ":
+		case "notice ": //a notice is similar to a PRIVMSG. Does not currently support users
 			send_room(p1, usr, fmt.Sprintf(":%s NOTICE %s :%s\r\n", usr.nickname, p1, mb))
-		case "explode ": 
+		case "explode ": //my custom command to cause the server to shutdown
 			if p1 == "Please" { os.Exit(0) }
-		case "quit ","quit":
+		case "quit ","quit": //quit is the command for a user to gracefully disconnect
 			close(usr.channel)
 		default: 
 			fmt.Printf("Don't know what to do with %s from '%s'\n", c, line)
 	}
 }
 
+//send a user-to-user message. usr is the source user. nick si the destination nickname
 func priv_msg_usr(usr *user, nick string, message string){
-	var r nick_req
+	var r nick_req //create the nickname service request object
 	r.nick=nick
 	r.usr = usr
 	r.search = true
-	r.retchan = make (chan bool, 1)
+	r.retchan = make (chan bool, 1) //buffer to ensure we get a response (see the nickservices non-blocking code)
 	r.uchan = make (chan *user, 1)
 	nickserv <- r
-	if <- r.retchan {
+	if <- r.retchan { //if the nick was found, create a message and send to the user
 		u := <- r.uchan
 		fmt.Println("User " + nick + " found for u2u private message -> "+u.nickname);
 		msg := get_msg()
@@ -439,16 +477,18 @@ func priv_msg_usr(usr *user, nick string, message string){
 	}
 }
 
+//Change the nickname. Create nick service object and send it
 func change_nick(usr *user, nick string) bool{
 	var r nick_req
 	r.nick=nick
 	r.usr = usr
 	r.search = false
-	r.retchan = make (chan bool, 1)
+	r.retchan = make (chan bool, 1) //buffer to ensure respone (see non-blocking code)
 	nickserv <- r
 	return <- r.retchan;
 }
 
+//Send a message to usr in the format used from server-to-client communications
 func server_msg(usr *user, code string, message string){
 	mb:=fmt.Sprintf(":server %s %s %s", code, usr.nickname, message)
 	fmt.Printf(">user %d: %s\n",usr.id, mb)
@@ -457,6 +497,7 @@ func server_msg(usr *user, code string, message string){
 	usr.channel <- msg
 }
 
+//exit all chatrooms. This is done on QUIT and on closed socket
 func part_all(usr *user){
 	for _, r := range usr.rooms {
 		fmt.Println(usr.nickname + " leaving " + r.name);
@@ -464,6 +505,7 @@ func part_all(usr *user){
 	}
 }
 
+//generate a random nickname. This was based on some code I found online but I've lost the reference to it
 func randNick() string{
 	var chars = []rune("abcdefghijklmnopqrstuvwxyz1234567890-");
 	nick:="user-"
@@ -474,6 +516,7 @@ func randNick() string{
 	return nick+string(b)
 }
 
+//Error checking function used by main
 func checkError(err error) {
         if err != nil {
                 fmt.Println("Fatal error ", err.Error())
